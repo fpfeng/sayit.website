f77db5787c79a48bcd3b5756dad5f8a55cf6ce3c
feng <187j3x1@gmail.com>
2016-08-26 16:24:00 +0800

    update markdown extensions

--- sayit/util/md_html.py
+++ sayit/util/md_html.py
@@ -2,4 +2,6 @@ from misaka import Markdown, HtmlRenderer
 
 
 render = HtmlRenderer(flags=('hard-wrap', 'escape'))
-to_html = Markdown(render, extensions=('fenced-code',))
+to_html = Markdown(render, extensions=('fenced-code',
+                                       'tables',
+                                       'no-intra-emphasis'))
